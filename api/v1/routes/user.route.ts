e74f873bd462c9d6177505e2275215064f269d5e
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-06 01:29:59 +0700

    Thêm router private

--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -1,12 +1,14 @@
 import { Router, Request, Response } from "express";
-const router: Router = Router();
-
 import * as controller from "../controllers/user.controller";
+
+import * as authMiddleware from "../middlewares/auth.middleware";
+
+const router: Router = Router();
 
 router.post("/register", controller.register);
 
 router.post("/login", controller.login);
 
-router.get("/detail/:id", controller.detail);
+router.get("/detail", authMiddleware.requireAuth, controller.detail);
 
 export const userRouters: Router = router;
