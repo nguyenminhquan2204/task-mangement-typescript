b5f15e960294cfffedae7cc5b6fed643b080c940
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-05 23:47:33 +0700

    Đổi trạng thái một công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -4,6 +4,7 @@ import Task from "../models/task.model";
 import paginationHelper from "../../../helpers/pagination";
 import searchHelper from "../../../helpers/search";
 
+// [GET] /api/v1/tasks
 export const index = async (req: Request, res: Response) => {
     // Find
     interface Find {
@@ -61,6 +62,7 @@ export const index = async (req: Request, res: Response) => {
     res.json(tasks);
 };
 
+// [GET] /api/v1/tasks/detail/:id
 export const detail = async (req: Request, res: Response) => {
     const id: string = req.params.id;
 
@@ -70,4 +72,26 @@ export const detail = async (req: Request, res: Response) => {
     });
 
     res.json(task);
+};
+
+// [PATCH] /api/v1/tasks/change-status/:id
+export const changeStatus = async (req: Request, res: Response) => {
+    try {
+        const id: string = req.params.id;
+        const status: string = req.body.status;
+
+        await Task.updateOne({ _id: id}, {
+            status: status
+        });
+
+        res.json({
+            code: 200,
+            message: "Cập nhập trạng thái thành công!"
+        });
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Không tồn tại!"
+        });
+    }
 };
