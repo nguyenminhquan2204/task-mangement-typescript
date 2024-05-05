2b0d355bb1787a90f07bd745b60a6dbf2edac33e
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-05 22:54:09 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+    // Find
     interface Find {
         deleted: boolean,
         status?: string
@@ -10,14 +11,24 @@ export const index = async (req: Request, res: Response) => {
 
     const find: Find = {
         deleted: false
-    };
+    }
 
     if(req.query.status) {
         // find["status"] = req.query.status;
         find.status = req.query.status.toString();
     }
+    // End find
 
-    const tasks = await Task.find(find);
+    // Sort
+    const sort = {};
+
+    if(req.query.sortKey && req.query.sortValue) {
+        const sortKey = req.query.sortKey.toString();
+        sort[sortKey] = req.query.sortValue;
+    }
+    // End sort
+
+    const tasks = await Task.find(find).sort(sort);
 
     res.json(tasks);
 };
