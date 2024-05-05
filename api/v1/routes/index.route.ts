d3cccc95f9b7fe38a9359573c46fb8f5a853d78e
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-06 00:58:32 +0700

    Tính năng đăng ký

--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -1,11 +1,14 @@
 import { Express } from "express";
 import { taskRouters } from "./task.route";
+import { userRouters } from "./user.route";
 
 const mainV1Routes = (app: Express): void => {
     const version = "/api/v1";
 
     app.use(version + "/tasks", taskRouters);
 
+    app.use(version + "/users", userRouters);
+    
 };
 
 export default mainV1Routes;
