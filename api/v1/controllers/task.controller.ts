f10c5601996cb366b1115491f9233eb2b11b1f8f
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-06 00:26:51 +0700

    Update Enum

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -99,12 +99,17 @@ export const changeStatus = async (req: Request, res: Response) => {
 // [PATCH] /api/v1/tasks/change-multi
 export const changeMulti = async (req, res) => {
     try {
+        enum Key {
+            STATUS = "status",
+            DELETE = "delete"
+        }
+
         const ids: string[] = req.body.ids;
         const key: string = req.body.key;
         const value: string = req.body.value;
 
         switch (key) {
-            case "status":
+            case Key.STATUS:
                 await Task.updateMany(
                     {
                         _id: { $in: ids }
@@ -120,7 +125,7 @@ export const changeMulti = async (req, res) => {
                 });
                 break;
             
-            case "delete":
+            case Key.DELETE:
                 await Task.updateMany(
                     {
                         _id: { $in: ids }
