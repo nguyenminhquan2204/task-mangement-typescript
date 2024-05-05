87f5bb7ae7023533351fcd66696f2b18516dcd3f
nguyenminhquan2204 <nguyenminhquantth@gmail.com>
2024-05-06 01:52:36 +0700

    Deploy code

--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -1,11 +1,12 @@
 import { Express } from "express";
 import { taskRouters } from "./task.route";
 import { userRouters } from "./user.route";
+import * as authMiddleware from "../middlewares/auth.middleware";
 
 const mainV1Routes = (app: Express): void => {
     const version = "/api/v1";
 
-    app.use(version + "/tasks", taskRouters);
+    app.use(version + "/tasks", authMiddleware.requireAuth, taskRouters);
 
     app.use(version + "/users", userRouters);
     
